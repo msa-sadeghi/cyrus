25a42681a763ed1e972411f1ce2cd87079b8beab
msa-sadeghi <sadeghi.msai@gmail.com>
2024-10-05 21:57:36 +0330

    a

--- castle/enemy.py
+++ castle/enemy.py
@@ -21,7 +21,56 @@ class Enemy(pygame.sprite.Sprite):
             
         self.image = self.images.get(self.animations[0])[0]
         self.rect = self.image.get_rect(topleft= (x,y))
+        self.image_number = 0
+        self.action = "walk"
+        self.last_animation_update_time = 0
         enemy_group.add(self)
+        self.touch = False
+        self.last_damage_time = 0
+        
+    def update(self, bullet_group, castle, all_enemy_types, all_enemies_damage):
+        if self.alive:
+            if not self.touch:
+                self.rect.x += self.speed
+            self.check_alive()
+            self.check_collisions(bullet_group, castle,all_enemy_types, all_enemies_damage)
+        self.animation()
+        
+    def check_collisions(self, bullet_group, castle,all_enemy_types, all_enemies_damage):
+        
+        if self.rect.colliderect(castle.rect):
+            self.touch = True
+            if pygame.time.get_ticks() - self.last_damage_time > 1000:
+                self.last_damage_time = pygame.time.get_ticks()
+                i = all_enemy_types.index(self.type)
+                castle.health -= all_enemies_damage[i]
+                if castle.health < 0:
+                    castle.health = 0
+            
+        if self.action == "death":
+            castle.money += 10
+        
+        if pygame.sprite.spritecollide(self, bullet_group, True):
+            self.health -= 25
+            if self.health <= 0:
+                self.health = 0
+    def check_alive(self):
+        if self.alive:
+            if self.health <= 0:
+                self.alive = False
+                self.action = "death"
+        
+    def animation(self):
+        self.image = self.images.get(self.action)[self.image_number]
+        if pygame.time.get_ticks() - self.last_animation_update_time > 100:
+            self.last_animation_update_time = pygame.time.get_ticks()
+            self.image_number += 1
+            if self.image_number >= len(self.images[self.action]):
+                if self.action != "death":
+                    self.image_number = 0
+                else:
+                    self.image_number = len(self.images[self.action]) - 1
+        
             
             
         
--- castle/main.py
+++ castle/main.py
@@ -21,38 +21,47 @@ font = pygame.font.SysFont("arial", 20)
 
 MAX_DIFFICAULTY = 1000
 level_difficulty = 0
-
+last_enemy_spawn_time = 0
 enemy_group = pygame.sprite.Group()
 
 all_enemy_types = ("knight", "goblin", "red_goblin", "purple_goblin")
 all_enemy_healths = (100, 125, 150,175)
 all_enemy_speeds = (1, 1, 2,2)
+all_enemies_damage = (10, 20, 30, 35)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
    
-    while level_difficulty < MAX_DIFFICAULTY:
-        i = randrange(len(all_enemy_types))
-        e = Enemy(
-                    all_enemy_types[i],
-                    all_enemy_healths[i],
-                    all_enemy_speeds[i],
-                    enemy_group,
-                    100, 
-                    300
-                  )
-        level_difficulty += all_enemy_healths[i]
+    
     print(len(enemy_group))    
     screen.blit(bg, (0,0))
+    
+    if level_difficulty < MAX_DIFFICAULTY:
+        if pygame.time.get_ticks() - last_enemy_spawn_time > 1000:
+            last_enemy_spawn_time = pygame.time.get_ticks()
+            i = randrange(len(all_enemy_types))
+            e = Enemy(
+                        all_enemy_types[i],
+                        all_enemy_healths[i],
+                        all_enemy_speeds[i],
+                        enemy_group,
+                        100, 
+                        400
+                    )
+            level_difficulty += all_enemy_healths[i]
+    
+    
+    
     Game.show_scoreboard(font, level, castle.money, castle.ammo, screen)
     castle.draw(screen)
     castle.shoot(bullet_group)
     bullet_group.update()
     bullet_group.draw(screen)
-    enemy_group.update()
+    enemy_group.update(bullet_group, castle, all_enemy_types, all_enemies_damage)
     enemy_group.draw(screen)
     castle_healthbar.draw(screen, castle.health)
+    print(castle.health)
     pygame.display.update()
     clock.tick(FPS)
